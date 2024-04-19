ab848c8d7d5b9e74d7b3fbb1d56b7c95d3e501a7
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-04-19 12:29:52 +0200

    working on Cognata dataset

--- script/get-dataset-cognata-mlcommons/customize.py
+++ script/get-dataset-cognata-mlcommons/customize.py
@@ -1,29 +1,6 @@
 from cmind import utils
 import os
-import shutil
-import sys
 
-
-def ask_user(question, default="yes"):
-    valid = ["yes", "y", "no", "n"]
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == "":
-            return default.startswith('y')
-        elif choice in valid:
-            return choice.startswith('y')
-        else:
-            print("Please input y/n\n")
 
 def preprocess(i):
 
@@ -31,19 +8,19 @@ def preprocess(i):
 
     script_path = i['run_script_input']['path']
 
-    with open(os.path.join(script_path, "license.txt"), "r") as f:
-        print(f.read())
-
-    response = ask_user("Do you accept?")
-
-    print(response)
+    input('xyz11')
 
 
     return {'return': 0}
 
 def postprocess(i):
     env = i['env']
-    return {'return': -1} #todo
+
+    input('xyz22')
+    
+    return {'return': 1, 'error':'TBD'} #todo
+
+    
     if env.get('CM_DATASET_CALIBRATION','') == "no":
         env['CM_DATASET_PATH_ROOT'] = os.path.join(os.getcwd(), 'install')
         env['CM_DATASET_PATH'] = os.path.join(os.getcwd(), 'install', 'validation', 'data')
