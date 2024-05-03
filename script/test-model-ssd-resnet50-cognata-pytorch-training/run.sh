39737208e48d7153524db77f8dca1986a68391ef
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-05-03 13:39:06 +0200

    fixed cognata dataset path on Linux

--- script/test-model-ssd-resnet50-cognata-pytorch-training/run.sh
+++ script/test-model-ssd-resnet50-cognata-pytorch-training/run.sh
@@ -4,5 +4,5 @@ echo "======================================================="
 
 cd ${CM_ML_MODEL_CODE_WITH_PATH}
       
-torchrun --nproc_per_node=1  train.py --model ssd --batch-size 1 --dataset Cognata --data-path %CM_DATASET_MLCOMMONS_COGNATA_PATH% --save-folder trained_models --config test_8MP --save-name test_8mp --epoch 2
+torchrun --nproc_per_node=1  train.py --model ssd --batch-size 1 --dataset Cognata --data-path ${CM_DATASET_MLCOMMONS_COGNATA_PATH} --save-folder trained_models --config test_8MP --save-name test_8mp --epoch 2
 test $? -eq 0 || exit $?
