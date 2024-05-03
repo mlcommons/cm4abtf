986dca3f0227b6982e13a15350157c5bb6af795d
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-05-03 18:15:58 +0200

    fixes

--- script/test-model-ssd-resnet50-cognata-pytorch-evaluation/run.sh
+++ script/test-model-ssd-resnet50-cognata-pytorch-evaluation/run.sh
@@ -7,7 +7,6 @@ export PYTHONPATH=${CM_ML_MODEL_CODE_WITH_PATH}:${PYTHONPATH}
 torchrun --nproc_per_node=1 \
    ${CM_TMP_CURRENT_SCRIPT_PATH}/src/evaluate.py \
    --model ${CM_ABTF_ML_MODEL_NAME} \
-   --batch-size ${CM_ABTF_ML_MODEL_TRAINING_BATCH_SIZE} \
    --dataset ${CM_ABTF_DATASET} \
    --data-path ${CM_DATASET_MLCOMMONS_COGNATA_PATH} \
    --pretrained-model ${CM_ABTF_ML_MODEL_TRAINING_STORAGE} \
